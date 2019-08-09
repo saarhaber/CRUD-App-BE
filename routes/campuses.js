704450b0fc6f7153c5e7dfff6d509ab4f6c751af
thomas-westfall <thomas.westfall64@myhunter.cuny.edu>
2019-08-09 13:50:58 -0400

    finished students resetful

--- routes/campuses.js
+++ routes/campuses.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const { Campus } = require('../database/models')
+const { Student } = require('../database/models')
 
 router.get("/", (req, res, next) => {
     Campus.findAll()
@@ -11,37 +12,38 @@ router.get("/:id", async function(req, res, next){
     let foundCampus;
 
     try {
-        foundCampus = await Campus.findOne({ where: { id: req.params.id} });
+        foundCampus = await Campus.findOne({where: { id: req.params.id}, include: [{model: Student}]})
     }
     catch (err){
         next(err);
     }
+    
     
     res.status(200).json(foundCampus);
 });
 
 
-router.get("/:id/students", async function(req, res, next){
-    let foundCampus;
+// router.get("/:id/students", async function(req, res, next){
+//     let foundCampus;
 
-    try {
-        foundCampus = await Campus.findOne({ where: { id: req.params.id} });
-    }
-    catch (err){
-        next(err);
-    }
+//     try {
+//         foundCampus = await Campus.findOne({ where: { id: req.params.id} });
+//     }
+//     catch (err){
+//         next(err);
+//     }
 
-    let studentsOfCampus;
+//     let studentsOfCampus;
 
-    try {
-        studentsOfCampus = await foundCampus.getStudents();
-    }
-    catch (err) {
-        next(err);
-    }
+//     try {
+//         studentsOfCampus = await foundCampus.getStudents();
+//     }
+//     catch (err) {
+//         next(err);
+//     }
     
-    res.status(200).json(studentsOfCampus);
-});
+//     res.status(200).json(studentsOfCampus);
+// });
 
 router.post("/", (req, res, next) => {
     let newCampus = req.body;
--- routes/students.js
+++ routes/students.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const { Student } = require('../database/models');
+const { Campus } = require('../database/models')
 
 router.get("/", (req, res, next) => {
     Student.findAll()
@@ -8,10 +9,55 @@ router.get("/", (req, res, next) => {
 });
 
 router.get("/:id", (req, res, next) => {
-    Student.findByPk(req.params.id)
-      .then(student => res.json(student))
-      .catch(next)
+  Student.findByPk(req.params.id, {include: [Campus]})
+    .then(student => res.json(student))
+    .catch(next)
 });
+
+router.post("/", (req, res, next) => {
+  let newStudent = req.body;
+  Student.create(newStudent);
+  res.status(200).json("Student created!");
+})
+
+//Puts modified student at end of json
+router.put("/:id", async function(req, res, next){
+
+  let foundStudent;
+
+  try {
+      foundStudent = await Student.findOne({ where: { id: req.params.id} });
+  }
+  catch (err){
+      next(err);
+  }
+
+  foundStudent.update(req.body);
+
+  res.status(200).send("Student updated!")
+
+})
+
+//does not update ids of rest of students
+//example, delete student at id 1, does not set reset of
+//students one back. its handled in the fe, but could have
+//unintended side effects (or not work entirely)
+router.delete("/:id", async function(req, res, next){
+
+  let foundStudent;
+
+  try {
+      foundStudent = await Student.findOne({ where: { id: req.params.id} });
+  }
+  catch (err){
+      next(err);
+  }
+
+  foundStudent.destroy();
+
+  res.status(200).send("Student deleted!")
+
+})
 
 // router.post("/", (req, res, next) => {
 //     let newStudent = req.body;
